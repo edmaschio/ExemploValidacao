ea33ddbae602ea34d4df3fb2e4146ab4abe83631
Ederson Maschio <edmaschio@gmail.com>
2016-02-01 21:08:06 -0200

    Exemplo de Validação MVC

---
+++ ExemploValidacao/Controllers/PessoaController.cs
@@ -0,0 +1,49 @@
+﻿using ExemploValidacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var bancoDeNomesDeExemplo = new Collection<string>
+            {
+                "Cleyton",
+                "Anderson",
+                "Ederson",
+                "Renata"
+            };
+
+            return Json(bancoDeNomesDeExemplo.All(x => x.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ExemploValidacao/Models/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "Nome deve ser preenchido")]
+        public string Nome { get; set; }
+
+        [StringLength(50, MinimumLength =5, ErrorMessage = "A observação deve ter entre 5 e 50 caracteres")]
+        [Required(ErrorMessage = "Observacão deve ser preenchida")]
+        public string Observacao { get; set; }
+
+        [Range(18,50, ErrorMessage = "A idade da pessoa deve ser entre 18 e 50 anos")]
+        public int Idade { get; set; }
+
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+        public string Email { get; set; }
+
+        [Remote("LoginUnico", "Pessoa", ErrorMessage = "Este nome de login já existe")]
+        [RegularExpression(@"[a-zA-z]{5,15}", ErrorMessage = "Login deve possuir somente letras e entre 5 a 15 caracteres")]
+        [Required(ErrorMessage = "O login deve ser preenchido")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "A senha deve ser informada")]
+        public string Senha { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
